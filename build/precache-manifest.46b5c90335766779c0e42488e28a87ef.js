33174512ef993383dc1383b420efe3dff6618f4b
David Steger <d.steger@ymail.com>
2019-08-12 21:23:17 -0700

    added hover animations

---
+++ build/precache-manifest.46b5c90335766779c0e42488e28a87ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d49c3e76ab1d3528778f34a1046711",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "9049fcf2943eb21d6271",
+    "url": "/Clicky-Game/static/css/main.15cc2b2a.chunk.css"
+  },
+  {
+    "revision": "be09d4f44607f9c8ff7e",
+    "url": "/Clicky-Game/static/js/2.c36c0770.chunk.js"
+  },
+  {
+    "revision": "9049fcf2943eb21d6271",
+    "url": "/Clicky-Game/static/js/main.35fbd86d.chunk.js"
+  },
+  {
+    "revision": "d95236dd8577f18ac0a3",
+    "url": "/Clicky-Game/static/js/runtime~main.6f0ca3e2.js"
+  }
+]);
